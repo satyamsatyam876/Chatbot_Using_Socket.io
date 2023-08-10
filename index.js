5df218ab68ab7fac853362d60eb607416cf593f6
Satyam <satyam.07srivastava2000@gmail.com>
2023-08-10 12:49:27 +0530

    Socket.io functionality Setup

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-const express = require('express')
-const port = 8000
-const app = express()
-const http = require('http').createServer(app)
+const express = require('express');
+const port = 5000;
 
+const app = express();
 
-app.get('/', (req, res) => {
+const http = require('http').createServer(app);
 
-    res.sendFile(__dirname + '/index.html')
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html');
 })
 
-http.listen(port, (err) => {
-    if (err) {
+http.listen(port, (err)=>{
+    if(err) {
         console.log(`${err}`)
     }
-    console.log(`server is connected to port ${port}`)
+    console.log(`Server is running on port : ${port}`)
 })
-//import the socket.io with the server
 
-const io = require('socket.io')(http)
+
+// Import the socket.io with the server
+const io = require('socket.io')(http);
+
+io.on('connection', (socket)=>{
+    console.log('Server has established the connection with the client');
+
+    // for recieving the message
+    socket.on("message", (msg)=>{
+        socket.broadcast.emit('message', msg)
+    })
+})
